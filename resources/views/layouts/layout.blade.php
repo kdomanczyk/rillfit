205ddf879b0b6fb1c53da12bc11a0de858eee7ee
kdomanczyk <41953542+kdomanczyk@users.noreply.github.com>
2018-07-31 14:24:00 +0200

    Initial commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,67 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="{{asset('css/app.css')}}" >  -->
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}" > 
+    
+    <title>Rillfit six!</title>
+  </head>
+  <body>
+
+
+  @yield('content')
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    wersja na githuba
+  </body>
+</html>
+
+
+<!-- <html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+       
+        
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}" >  
+        <link rel="stylesheet" href="{{asset('css/custom.css')}}" >
+        
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="{{asset('js/script.js')}}" ></script>
+        <script src="{{asset('js/app.js')}}" ></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        
+
+    </head>
+    <body>
+     @yield('content')
+    </body>
+
+</html> -->
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="container-fluid first-nav-fluid">
+        <div class="container first-nav">
+
+        </div>
+    </div>
+    <div class="container-fluid main-nav-fluid">
+        <div class="container main-nav">
+            <div class="nav-bar-menu">
+            
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand " href="#"><img src="{{asset('images/Rillfit-website.png')}}"></a>
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"><i>&#x21CC;</i></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav mx-auto">
+                        <a class="nav-item nav-link" href="#">Dane technicznes</a>
+                        <a class="nav-item nav-link" href="#">Stanowisko</a>
+                        <a class="nav-item nav-link" href="#">Akcesoria</a>
+                        <a class="nav-item nav-link" href="#">Ewolucja Rillfit SIX®</a>
+                        <a class="nav-item nav-link" href="#">Dystrybucja</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="main-wrapper">
+                        <h1>Nacinarka RILLFIT SIX® do pogłębiania bieżnika opon</h1>
+                        <h2>Dostępna już w polsce!
+                        <span>Najlepsza nacinarka na rynku niemieckim!</span>
+                        </h2>
+                        
+                        <a href="https://sklep.phu-szczepan.pl/product-pol-848-Nacinarka-do-Opon-RILLFIT-SIX-Wyrzynarka-do-Bieznika-RUFF-Nozyki.html"><button class="btn orange" type="button"><span>Kup już teraz!</span></button></a>
+                        <div class="yt-frame">
+                         <iframe width="853" height="480" src="https://www.youtube.com/embed/mE7RurYJOdw" frameborder="0" allowfullscreen=""></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid main-content-container-fluid">
+        <div class="container first-content-container">
+            <div class="row">
+                <div class="col-md-12">
+                     <h1>RILLFIT SIX®</h1>
+                     <p>Poznaj innowacyjną nacinarke do bieżnika niemieckiej firmy RUFF. To nowoczesne rozwiązania, które zwiększają komfort użytkowania i niezawodność urządzenia. Zobacz, w jaki sposób ta nacinarka zapewni Ci znacznie bardziej komfortową pracę przy rekonstrukcji bieżnika. 
+                    </p>
+
+               
+                    <div class="content-first">
+                        <div class="row">
+                            
+                            <div class="col-md-6  tab-flex-container order-md-last tab-flex-img">
+                                <div class="tab-flex">
+                                    <img src="images/180006_02.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-6    tab-flex-container tab-flex-text">
+                                <div class="tab-flex">
+                                <h2>Transformator HI-TECH</h2> Zabezpieczenie termiczne, które chroni przed przegrzaniem. Eliminuje ryzyko przegrzania. Nie ma konieczności przerw na studzenie - lepsza wydajność pracy.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            
+                            <div class="col-md-6  tab-flex-container tab-flex-img">
+                                <div class="tab-flex">
+                                    <img src="images/180006_03.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-6 tab-flex-container  tab-flex-text">
+                                <div class="tab-flex">
+                                <h2>Regulacja siły grzania</h2> Cztery stopnie regulacji siły grzania pozwolą Ci łatwo dostosować parametry nacinarki do aktualnych potrzeb.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 col-md-pull-4 order-md-last tab-flex-container tab-flex-img">
+                                <div class="tab-flex">
+                                    <img src="images/180006_05.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-6 tab-flex-container  tab-flex-text">
+                                <div class="tab-flex">
+                                <h2>Komfortowy uchwyt</h2> Ergonomiczna rączka. Długi kabel ze wzmocnionymi zakończeniami. Wygodna praca z urządzeniem w trakcie rekonstrukcji bieżnika.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                        <div class="col-md-6 tab-flex-container tab-flex-img">
+                                <div class="tab-flex">
+                                    <img src="images/180006_01.png" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-6 tab-flex-container  tab-flex-text">
+                                <div class="tab-flex">
+                                <h2>Wytrzymała obudowa</h2> Odporna na wodę, wykonana z trwałego tworzywa. Posiada otwór umożliwiający przymocowanie transformatora na ścianie, podłożu lub nawet innym urządzeniu. 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="buy-device">
+                            <p>Tę nacinarkę kupisz na:</p>
+                            <a href="https://sklep.phu-szczepan.pl/product-pol-848-Nacinarka-do-Opon-RILLFIT-SIX-Wyrzynarka-do-Bieznika-RUFF-Nozyki.html"><button class="btn orange" type="button"><span>Kup już teraz!</span></button></a>
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid second-content-fluid">
+            <div class="div container second-content-container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h2>Jak działają ostrza?</h2>
+                        <h3>Zobacz, jak to działa:</h3>
+                        <ol>
+                            <li>Przykładasz nożyk do nacinanego materiału</li>
+                            <li>Prąd przepływa przez nożyki.</li>
+                            <li>Odciągasz nożyk - prąd przestaje płynąć.</li>
+                        </ol>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+asdsad
